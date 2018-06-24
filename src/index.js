c277932877c6344c6daf29f16631998a67fdb0a8
Yomar Miranda <mirandayomar@gmail.com>
2018-06-23 19:28:47 -0500

    Configurando Firebase.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+firebase.initializeApp({
+	apiKey: "AIzaSyCk5WafsDdWzj1-FvW2wJrIvshxJtyC_W8",
+    authDomain: "app-react-firebase.firebaseapp.com",
+    databaseURL: "https://app-react-firebase.firebaseio.com",
+    projectId: "app-react-firebase",
+    storageBucket: "app-react-firebase.appspot.com",
+    messagingSenderId: "271296205604"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
