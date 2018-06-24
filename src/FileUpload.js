e28265f44010b06625e4b47370fd530451ebc7a3
Yomar Miranda <mirandayomar@gmail.com>
2018-06-23 21:42:10 -0500

    Componente FileUpload - Componente para subir archivos, en este caso imagenes.

---
+++ src/FileUpload.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+
+class FileUpload extends Component{
+	constructor(){
+		super();
+		this.state = {
+			uploadValue: 0,
+			picture: null
+		}
+	}
+
+	render(){
+		return(
+			<div>
+				<progress value={this.state.uploadValue} max="100"></progress> <br />
+				<input type="file" onChange={this.props.onUpload} /> <br />
+				<img width="320" src={this.state.picture} alt="" />
+			</div>
+		)
+	}
+}
+
+export default FileUpload;
