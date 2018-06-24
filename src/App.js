8948824d28411e8be09d9ce633408111c8f8e79c
Yomar Miranda <mirandayomar@gmail.com>
2018-06-23 20:20:53 -0500

    Configurando la autenticación en nuestra App a través de Google.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
+
 import './App.css';
 
 class App extends Component {
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión.`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Mini Gram</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.handleAuth}>Login con Google</button>
         </p>
       </div>
     );
