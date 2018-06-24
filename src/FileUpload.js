fd3adf99ee383d86ad7a68a9913043b9ff1e6ed5
Yomar Miranda <mirandayomar@gmail.com>
2018-06-23 23:39:44 -0500

    Subir archivos y mostrar progreso de carga, aún falta mostrar la imagen al cargarla.

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -8,13 +8,49 @@ class FileUpload extends Component{
 			uploadValue: 0,
 			picture: null
 		}
+
+		this.handleUpload = this.handleUpload.bind(this)
+	}
+
+	handleUpload(event){
+		/**
+		 * Archivo enviado a través del formulario.
+		 */
+		const file = event.target.files[0];
+
+		/**
+		 * Diretorio en el storage de firebase en donde vamos a subir los archivos.
+		 */
+		const storageRef = firebase.storage().ref(`/fotos/${file.name}`); 
+		
+		/**
+		 * Subir el archivo al storage.
+		 */
+		const task = storageRef.put(file);
+
+		/**
+		 * Metodo de firebase que se llama a medida que el archivo se vaya subiendo.
+		 */
+		task.on('state_changed', snapshot => {
+			let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+			this.setState({
+				uploadValue: percentage
+			})
+		}, error => {
+			console.log(`Error: ${error.message}`);
+		}, (/* Cuando el archivo se ha subido por completo */) => {
+			this.setState({
+				uploadValue: 100,
+				picture: task.snapshot.downloadURL
+			})
+		})
 	}
 
 	render(){
 		return(
 			<div>
-				<progress value={this.state.uploadValue} max="100"></progress> <br />
-				<input type="file" onChange={this.props.onUpload} /> <br />
+				<progress value={this.state.uploadValue} max="100">{this.state.uploadValue}</progress> <br />
+				<input type="file" onChange={this.handleUpload} /> <br />
 				<img width="320" src={this.state.picture} alt="" />
 			</div>
 		)
